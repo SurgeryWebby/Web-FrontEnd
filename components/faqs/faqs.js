14df28085c46e8fafd16917daab4789c6a4cec99
nworathanara <Napapol.Worathanarat@agoda.com>
2018-10-27 15:50:34 +0700

    add fag page

---
+++ components/faqs/faqs.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Accordion, Icon, Container } from 'semantic-ui-react';
+import { Header } from './StyledComponents/FaqsStyledComponents';
+
+
+export default () => (
+    <Container fluid>
+      <Container fluid>Hello Header</Container>
+      <Container text>
+        <p>
+          Hello Body
+        </p>
+      </Container>
+    </Container>
+)
