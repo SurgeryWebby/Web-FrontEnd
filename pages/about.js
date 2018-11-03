dc638ce2a40e517ae535fd5b1b4c9fe6f82471d8
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-03 18:19:40 +0700

    add about us rows

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Card, Icon, Image, Grid } from 'semantic-ui-react'
 
 const AboutUs = () => (
     <div>
@@ -17,9 +17,37 @@ const AboutUs = () => (
 ตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันMicrofat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน
             </p>
         </Container>
-        <Container fluid textAlign='center'>
-            
-            Why us
+        <Container textAlign='center'>
+            <Header>Why us ?</Header>
+            <Grid column={3} columns='equal'>
+                <Grid.Row textAlign='center'>
+                    <Grid.Column>
+                        <Card>
+                            <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+                            <Card.Content>
+                                <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Card>
+                            <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+                            <Card.Content>
+                                <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Card>
+                            <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
+                            <Card.Content>
+                                <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                </Grid.Row>
+                
+            </Grid>
         </Container>
         {/* Banner */}
         {/* Video */}
