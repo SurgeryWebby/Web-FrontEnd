90b72737c66a18b773646a009be2493a40407681
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-04 22:10:49 +0700

    try menu

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import { Menu, Segment, Container, Grid, Button, Divider, Icon, Label, Image } from 'semantic-ui-react'
 
 const HOME = 'Home'
 const PACKAGE = 'Package'
 const BLOG = 'Blog'
 const ABOUTUS = 'About Us'
 const FAQ = 'FAQ'
+
+const menuStyle = {
+  borderColor: "white",
+  marginTop: "0px"
+}
+
+const topMenuStyle = {
+  backgroundColor: "#857664",
+  marginBottom: "0px"
+}
+
+const loginStyle = {
+  backgroundColor: "#C39D5C",
+  marginTop: "10px",
+  marginBottom: "10px"
+}
 
 class MenuDefault extends Component {
     state = { activeItem: 'home' }
@@ -17,15 +33,31 @@ class MenuDefault extends Component {
   
       return (
         <div>
-          <Menu pointing secondary>
-            <Menu.Item name={HOME} active={activeItem === HOME} onClick={this.handleItemClick} />
-            <Menu.Item name={PACKAGE} active={activeItem === PACKAGE} onClick={this.handleItemClick} />
-            <Menu.Item name='Blog' active={activeItem === 'Blog'} onClick={this.handleItemClick} />
-            <Menu.Item name='About Us' active={activeItem === 'About Us'} onClick={this.handleItemClick} />
-            <Menu.Item name='FAQ' active={activeItem === 'FAQ'} onClick={this.handleItemClick} />
-            <Menu.Menu position='right'>
-              <Menu.Item name='Logout' active={activeItem === 'Logout'} onClick={this.handleItemClick} />
-            </Menu.Menu>
+          <Menu borderless size='mini' style={topMenuStyle}>
+            <Container>
+              <Menu.Item>081-2345678</Menu.Item>
+              <Menu.Item><Icon link name='facebook f' inverted></Icon></Menu.Item>
+              <Menu.Menu position='right'>
+                <Menu.Item name='EN' />
+                <Menu.Item name='TH' />
+              </Menu.Menu>
+            </Container>
+          </Menu>
+          <Menu pointing secondary borderless size='small' style={menuStyle}>
+            <Container>
+              <Menu.Item header>
+                <Image size='mini' src='/static/menu/logo.png' style={{ marginRight: '1.5em' }} />
+              </Menu.Item>
+              <Menu.Item name={HOME} active={activeItem === HOME} onClick={this.handleItemClick} />
+              <Menu.Item name={PACKAGE} active={activeItem === PACKAGE} onClick={this.handleItemClick} />
+              <Menu.Item name='Blog' active={activeItem === 'Blog'} onClick={this.handleItemClick} />
+              <Menu.Item name='About Us' active={activeItem === 'About Us'} onClick={this.handleItemClick} />
+              <Menu.Item name='FAQ' active={activeItem === 'FAQ'} onClick={this.handleItemClick} />
+              
+              <Menu.Menu position='right'>
+                <Button size='small' style={loginStyle}>Log In</Button>
+              </Menu.Menu>
+            </Container>
           </Menu>
         </div>
       )
--- pages/about.js
+++ pages/about.js
@@ -3,63 +3,74 @@ import 'semantic-ui-css/semantic.min.css'
 import { Container, Header, Card, Icon, Image, Grid } from 'semantic-ui-react'
 import MenuDefault from '../components/menu/menu.js'
 
+const containerStyle = {
+    marginBottom: "50px"
+}
+
+const gridStyle = {
+    backgroundImage: "/static/about/service-background.png",
+    marginTop: "10px"
+}
+
 const AboutUs = () => (
     <div>
         <MenuDefault />
-        {/* Fucking navbar */}
-        <Container fluid>
-            <Image src='/static/about/header.png' fluid/>
-        </Container>
-        <Container fluid>
-            <Image src='/static/about/video.png' centered/>
-        </Container>
-        <Container fluid text textAlign='center'>
-            <Header>หน้าวีสวยอย่างเป็นธรรมชาติ</Header>
-            <p>
-            Microfat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน ของตนเองมาผสมกับสเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมัน ของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของ
-ตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมัน
-            </p>
-            <p>
-            Microfat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน ของตนเองมาผสมกับสเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมัน ของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของ
-ตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันMicrofat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน
-            </p>
-        </Container>
-        <Container textAlign='center'>
-            <Header>Why us ?</Header>
-            <Grid column={3} columns='equal'>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Card centered>
-                            <Image src='/static/about/whyus.png'/>
-                            <Card.Content>
-                                <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card centered>
-                            <Image src='/static/about/whyus.png'/>
-                            <Card.Content>
-                                <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Card centered>
-                            <Image src='/static/about/whyus.png'/>
-                            <Card.Content>
-                                <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                </Grid.Row>
-                
-            </Grid>
-        </Container>
-        {/* footer */}
-        <Container>
+        <Grid style={gridStyle}>
+            <Container fluid style={containerStyle}>
+                <Image src='/static/about/banner-background.png' fluid/>
+            </Container>
+            <Container fluid>
+                <Image src='/static/about/video.png' centered/>
+            </Container>
+            <Container fluid text textAlign='center'>
+                <Header>หน้าวีสวยอย่างเป็นธรรมชาติ</Header>
+                <p>
+                Microfat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน ของตนเองมาผสมกับสเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมัน ของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของ
+    ตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมัน
+                </p>
+                <p>
+                Microfat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน ของตนเองมาผสมกับสเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมัน ของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของ
+    ตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันMicrofat คือ การนำไขมันของตนเองมาผสมกับสเต็มเซลล์ และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้าหรือเติมเต็มสัดส่วนต่างๆในร่างกาย Microfat คือการนำไขมันของตนเองมาผสมกับ สเต็มเซลล์และฉีดเติมเต็มกลับเข้าไปสู่บริเวณใบหน้า หรือเติมเต็ม Microfat คือการนำไขมัน
+                </p>
+            </Container>
+            <Container textAlign='center'>
+                <Header>Why us ?</Header>
+                <Grid column={3} columns='equal'>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Card centered>
+                                <Image src='/static/about/whyus.png'/>
+                                <Card.Content>
+                                    <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Card centered>
+                                <Image src='/static/about/whyus.png'/>
+                                <Card.Content>
+                                    <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Card centered>
+                                <Image src='/static/about/whyus.png'/>
+                                <Card.Content>
+                                    <Card.Header>แพทย์มือหนึ่ง เชี่ยวชาญ เชื่อถือได้</Card.Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    </Grid.Row>
+                    
+                </Grid>
+            </Container>
+            {/* footer */}
+            <Container>
 
-        </Container>
+            </Container>
+        </Grid>
+        
     </div>
 )
 
