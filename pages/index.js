588ca3512270689faa765cc5bd7735188d304063
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-09 02:07:04 +0700

    add font

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import Home from '../components/home/home.js'
 
 const Index = () => (
   <div>
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Chonburi|Prompt');
+    </style>
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/butler" type="text/css"/> 
     <MenuDefault activeItem='Home' />
     <Home />
     <FooterDefault/>
