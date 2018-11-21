a1914498760de9a08230e2b38fc18a61e96873ca
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-22 02:39:58 +0700

    add sample test

---
+++ test/main/common/TopWavySvg.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import TopWavySvg from '../../../src/main/common/TopWavySvg';
+
+describe('TopWavySvg renders', () => {
+    it('renders', () => {
+        const wrapper = shallow(<TopWavySvg />);
+        const expectedResult = <div>Hi</div>;
+        expect(wrapper.contains(expectedResult)).to.equal(true);
+    });
+});
