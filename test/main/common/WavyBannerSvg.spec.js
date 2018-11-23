ee3a7197c2920530bd71ce6c1a8ce552ba22a4f0
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-24 01:14:06 +0700

    add test for WavyBannerSvg

--- test/main/common/WavyBannerSvg.spec.js
+++ test/main/common/WavyBannerSvg.spec.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import TopWavySvg from '../../../src/main/common/TopWavySvg';
+import WavyBannerSvg from '../../../src/main/common/WavyBannerSvg';
 
-describe('TopWavySvg renders', () => {
+describe('WavyBannerSvg', () => {
     it('should render svg', () => {
-        const wrapper = shallow(<TopWavySvg />);
+        const wrapper = shallow(<WavyBannerSvg />);
         const expectedResult = (
             <svg
                 viewBox="0 0 500 150"
@@ -13,11 +13,12 @@ describe('TopWavySvg renders', () => {
                 style={{ height: '100%', width: '100%' }}
             >
                 <path
-                    d="M-103.86,123.84 C212.46,-145.55 323.25,337.00 590.00,-10.36 L592.20,230.42 L-12.02,187.00 Z"
-                    style={{ stroke: 'none', fill: '#FFF' }}
+                    d="M-26.60,146.53 C192.05,36.02 283.16,217.59 541.91,116.94 L516.39,170.22 L0.00,150.00 Z"
+                    style={{ stroke: 'none', fill: 'white' }}
                 />
             </svg>
         );
+
         expect(wrapper.contains(expectedResult)).to.equal(true);
     });
 });
