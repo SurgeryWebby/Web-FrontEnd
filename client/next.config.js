e916bde60cfcf83d1b9f4e97a2e25398328a0724
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-27 03:28:57 +0700

    update webpack

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,47 @@
-const withCss = require('@zeit/next-css');
+const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === 'development' ? {} : require('next-server/constants');
 
-module.exports = withCss({
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000,
-                    publicPath: './',
-                    outputPath: 'static/',
-                    name: '[name].[ext]',
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        return Object.assign(
+            {},
+            defaultConfig,
+            {
+                webpack(config) {
+                    config.module.rules.push({
+                        test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+                        use: {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 100000,
+                                publicPath: './',
+                                outputPath: 'static/',
+                                name: '[name].[ext]',
+                            },
+                        },
+                    });
+
+                    return config;
                 },
             },
-        });
+        );
+    }
+    const withCSS = require('@zeit/next-css');
+    return withCSS({
+        webpack(config) {
+            config.module.rules.push({
+                test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000,
+                        publicPath: './',
+                        outputPath: 'static/',
+                        name: '[name].[ext]',
+                    },
+                },
+            });
 
-        return config;
-    },
-});
+            return config;
+        },
+    });
+};
