8d11ca8abf164653212aab0b5c3086cb8420fa5a
Pasakorn Promsuban <paspro3@gmail.com>
2018-11-28 01:36:56 +0700

    try change webpack

--- client/next.config.js
+++ client/next.config.js
@@ -2,29 +2,26 @@ const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === 'development' ? {} 
 
 module.exports = (phase, { defaultConfig }) => {
     if (phase === PHASE_PRODUCTION_SERVER) {
-        return Object.assign(
-            {},
-            defaultConfig,
-            {
-                webpack(config) {
-                    config.module.rules.push({
-                        test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-                        use: {
-                            loader: 'url-loader',
-                            options: {
-                                limit: 100000,
-                                publicPath: './',
-                                outputPath: 'static/',
-                                name: '[name].[ext]',
-                            },
+        return {
+            webpack: function(config) {
+                config.module.rules.push({
+                    test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+                    use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 100000,
+                            publicPath: './',
+                            outputPath: 'static/',
+                            name: '[name].[ext]',
                         },
-                    });
+                    },
+                });
 
-                    return config;
-                },
+                return config;
             },
-        );
+        };
     }
+
     const withCSS = require('@zeit/next-css');
     return withCSS({
         webpack(config) {
